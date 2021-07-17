7337e370cfa56e8b8e5e5c25d22a877b4e1476eb
Benjamin PH <benjamin.phamho@gmail.com>
2021-07-17 19:59:51 +1000

    deploy

--- hackathon/hackathon/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from forum.models import Post
-
-def index(request):
-    # Get all posts
-    posts = Post.objects.all()
-    
-    return render(request, 'index.html', {'posts': posts})
--- hackathon/settings.py
+++ hackathon/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'forum.apps.ForumConfig',
+    'crispy_forms',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,6 +125,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
