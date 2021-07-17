6c3ffb407ca93efdae7d0d0d582bfa73b22b7cee
David Nguyen <david.nguyen3820@gmail.com>
2021-07-17 15:25:30 +1000

    finished authentication system

--- hackathon/hackathon/urls.py
+++ hackathon/hackathon/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as django_views
+from django.urls import include, path
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', user_views.home),
+    path('', include('users.urls'))
 ]
