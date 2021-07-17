5fc237372c79cc01933db91b8fdcfb11d6bfd581
David Nguyen <david.nguyen3820@gmail.com>
2021-07-18 02:50:37 +1000

    new env vars

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8!a^cat+-^e&2f!x1m+h6@)+r=w4td2qiv3mibo76(zom&4&yx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
