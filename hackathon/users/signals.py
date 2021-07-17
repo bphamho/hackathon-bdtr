f91fdf57131ab701eb38e2314ec17ea1230dbadc
David Nguyen <david.nguyen3820@gmail.com>
2021-07-17 20:59:11 +1000

    register fixed

--- hackathon/users/signals.py
+++ hackathon/users/signals.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-  
->>>>>>> 5dbb1683cf5bd2c2ac0bbb2b7ad7f262778d732f
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -14,8 +10,4 @@ def create_userprofile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_userprofile(sender, instance, **kwargs):
-<<<<<<< HEAD
     instance.userprofile.save()
-=======
-    instance.userprofile.save()
->>>>>>> 5dbb1683cf5bd2c2ac0bbb2b7ad7f262778d732f
