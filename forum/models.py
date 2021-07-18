8e0fdcf3970a458b893db3aff5adf163bc578017
Tim Huang <tim.huang20@hotmail.com>
2021-07-18 10:07:49 +1000

    moving files our files out of hackathon

--- forum/models.py
+++ forum/models.py
@@ -15,6 +15,11 @@ class Comment(models.Model):
     comment = models.CharField(max_length = 400)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+#class Article(models.Model):
+    #author = models.CharField(max_length = 200)
+    #write_date = models.DateTimeField('date published')
+    
+
 class Video(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length = 200)
