9140b920fe11faf463420103f9995cb9be14d432
Benjamin PH <benjamin.phamho@gmail.com>
2021-07-18 10:06:16 +1000

    moved

--- hackathon/community/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import CommunityDetail
-
-admin.site.register(CommunityDetail)
--- hackathon/community/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import CommunityDetail
-from django import forms
-
-class CreateCommunityForm(forms.ModelForm):
-    class Meta:
-        model = CommunityDetail
-        fields = ['name', 'description', 'image']
